de94b92e66fd3e21f53b7aa6293ab9f17df628cd
jp-reynolds <jpreynolds215@gmail.com>
2018-04-16 08:40:50 -0600

    commit: HW

---
+++ db/pizza.js
@@ -0,0 +1,51 @@
+const pizza = [
+  {
+    id: 1,
+    flavor: 'Pepperoni',
+    flavorKey: 'pepperoni',
+    location: 'New York',
+    locationKey: 'nyc',
+    image: '/static/img/pepperoni.jpg',
+  },
+  {
+    id: 2,
+    flavor: 'cheese',
+    flavorKey: 'cheese',
+    location: 'New York',
+    locationKey: 'nyc',
+    image: '/static/img/cheese.jpg',
+  },
+  {
+    id: 3,
+    flavor: 'sausage',
+    flavorKey: 'sausage',
+    location: 'New York',
+    locationKey: 'nyc',
+    image: '/static/img/sausage.jpg',
+  },
+  {
+    id: 4,
+    flavor: 'ham & mushroom',
+    flavorKey: 'ham-mushroom',
+    location: 'Italy',
+    locationKey: 'italy',
+    image: '/static/img/ham-mushroom.jpg',
+  },
+  {
+    id: 5,
+    flavor: 'deep dish',
+    flavorKey: 'deep-dish',
+    location: 'Chicago',
+    locationKey: 'chicago',
+    image: '/static/img/deep-dish.jpg',
+  },
+  {
+    id: 6,
+    flavor: 'pineapple',
+    location: 'Hawaii',
+    locationKey: 'hawaii',
+    image: '/static/img/pineapple.jpg',
+  }
+];
+
+module.exports = pizza;
