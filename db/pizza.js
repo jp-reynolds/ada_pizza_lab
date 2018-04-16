b4c4f91f8a0bd441de9738e5c595df56ba81ebb7
jp-reynolds <jpreynolds215@gmail.com>
2018-04-16 10:05:57 -0600

    updated pizza.js

--- db/pizza.js
+++ db/pizza.js
@@ -1,6 +1,5 @@
 const pizza = [
   {
-    id: 1,
     flavor: 'Pepperoni',
     flavorKey: 'pepperoni',
     location: 'New York',
@@ -8,7 +7,6 @@ const pizza = [
     image: '/static/img/pepperoni.jpg',
   },
   {
-    id: 2,
     flavor: 'cheese',
     flavorKey: 'cheese',
     location: 'New York',
@@ -16,7 +14,6 @@ const pizza = [
     image: '/static/img/cheese.jpg',
   },
   {
-    id: 3,
     flavor: 'sausage',
     flavorKey: 'sausage',
     location: 'New York',
@@ -24,7 +21,6 @@ const pizza = [
     image: '/static/img/sausage.jpg',
   },
   {
-    id: 4,
     flavor: 'ham & mushroom',
     flavorKey: 'ham-mushroom',
     location: 'Italy',
@@ -32,7 +28,6 @@ const pizza = [
     image: '/static/img/ham-mushroom.jpg',
   },
   {
-    id: 5,
     flavor: 'deep dish',
     flavorKey: 'deep-dish',
     location: 'Chicago',
@@ -40,7 +35,6 @@ const pizza = [
     image: '/static/img/deep-dish.jpg',
   },
   {
-    id: 6,
     flavor: 'pineapple',
     location: 'Hawaii',
     locationKey: 'hawaii',
